8ccb872d71040341c4dcba2c28d66864ffcacad7
Duc Dung <ducdung@Dung-Mac-mini.local>
2025-01-07 14:32:16 +0700

    page 404

--- 404.php
+++ 404.php
@@ -4,16 +4,16 @@ Template Name: 404 Template
 */
 ?>
 <?php get_header();
-get_template_part( 'framework/templates/site', 'titlebar');
 ?>
-
 <main id="bt_main" class="bt-site-main">
 	<div class="bt-main-content-ss">
 		<div class="bt-container">
-			<h2><?php esc_html_e('404 Error', 'awakenur'); ?></h2>
-			<h3><?php esc_html_e('Sorry! The Page Not Found ;(', 'awakenur'); ?></h3>
-			<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'awakenur'); ?></p>
-			<?php get_search_form(); ?>
+			<h2><?php esc_html_e('Oops!', 'awakenur'); ?></h2>
+			<h3><?php esc_html_e('Something is Missing....', 'awakenur'); ?></h3>
+			<p><?php esc_html_e('The page you are looking for cannot be found. take a break before trying again', 'awakenur'); ?></p>
+			<div class="bt-button-home bt-button-hover">
+				<a class="button" href="<?php echo esc_url(home_url('/')) ?>"><?php esc_html_e('Back To Homepage', 'awakenur'); ?></a>
+			</div>
 		</div>
 	</div>
 </main><!-- #main -->
